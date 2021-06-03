6f081c10aacbe86aca82bf629c4cd984038e9dae
Starman <30315137+Starman3787@users.noreply.github.com>
2021-06-03 21:38:51 +0100

    add queue system

--- src/request/index.js
+++ src/request/index.js
@@ -15,35 +15,7 @@ class RequestHandler {
 
         this.endpoints = endpoints;
 
-    }
-
-    tryRequests() {
-
-        let counter = 0;
-        
-        while (counter < this.requestQueue.length) {
-            console.log(2);
-            if (this.token) {
-                console.log(1);
-                if (this.bucket[this.endpoints[this.requestQueue[counter].request].bucket]?.remaining == 0 && new Date().getTime() < this.bucket[this.endpoints[this.requestQueue[counter].request]]?.reset) {
-
-                    const requestItem = this.requestQueue.shift();
-                    this.requestQueue.push(requestItem);
-
-                } else {
-                    console.log(this.requestQueue[counter]);
-                    const requestToMake = this.requestQueue[counter];
-                    this.requestQueue.splice(counter, 1);
-                    Promise.all([this.makeRequest(requestToMake)]);
-
-                }
-
-                counter++;
-
-
-            }
-
-        }
+        this.inProgress = [];
 
     }
 
@@ -58,45 +30,52 @@ class RequestHandler {
 
     }
 
-    addRequest(req, params, body) {
+    nextRequest() {
 
-        this.requestQueue.push({
-            request: req,
-            params: params,
-            body: body
-        });
+        if (this.requestQueue.length != 0) {
 
-        this.tryRequests();
+            const nextRequest = this.requestQueue.shift();
+
+            this.makeRequest(nextRequest.request, nextRequest.params, nextRequest.body);
+
+        }
 
     }
 
-    async makeRequest(to) {
+    async makeRequest(request, params, body) {
 
-        // if (this.bucket[to.bucket]?.remaining == 0 && new Date().getTime() < this.bucket[to.bucket]?.reset) {
+        if (!this.inProgress.includes(request) && !(this.bucket[this.endpoints[request].bucket]?.remaining == 0 && new Date().getTime() < this.bucket[this.endpoints[request]]?.reset)) {
 
+            this.inProgress.push(request);
 
+            const actualRequest = this.endpoints[request];
 
-        // }
+            const res = await fetch(`${this.requestURL}${actualRequest.path()}`, {
+                method: actualRequest.method,
+                headers: {
+                    "Authorization": this.authorization
+                }
+            });
+            console.log(res.status);
+            const json = await res.json();
+            // console.log(res.headers);
+            console.log(json);
+    
+            this.handleBucket(request, res.headers.get("x-ratelimit-bucket"), res.headers.get("x-ratelimit-remaining"), res.headers.get("x-ratelimit-reset"));
 
-        // // while () {
+            this.inProgress.splice(this.inProgress.indexOf(request), 1);
 
+            this.nextRequest();
 
-        // // }
+        } else {
 
-        const actualRequest = this.endpoints[to.request];
+            this.requestQueue.push({
+                request: request,
+                params: params,
+                body: body
+            });
 
-        const res = await fetch(`${this.requestURL}${actualRequest.path()}`, {
-            method: actualRequest.method,
-            headers: {
-                "Authorization": this.authorization
-            }
-        });
-        console.log(res.status);
-        const json = await res.json();
-        // console.log(res.headers);
-        console.log(json);
-
-        this.handleBucket(to.request, res.headers.get("x-ratelimit-bucket"), res.headers.get("x-ratelimit-remaining"), res.headers.get("x-ratelimit-reset"));
+        }
 
     }
 
