ba12bee630943b5df44c175daed4e38262fa43f9
Starman <30315137+Starman3787@users.noreply.github.com>
2021-06-05 02:10:08 +0100

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,26 @@
 module.exports.BASE_URL = 'https://discord.com/api';
 module.exports.VERSION = 9;
+module.exports.NAME = "gluon";
+module.exports.INTENTS = {
+    GUILDS: 0,
+    GUILD_MEMBERS: 1,
+    GUILD_BANS: 2,
+    GUILD_EMOJIS: 3,
+    GUILD_INTEGRATIONS: 4,
+    GUILD_WEBHOOKS: 5,
+    GUILD_INVITES: 6,
+    GUILD_VOICE_STATES: 7,
+    GUILD_PRESENCES: 8,
+    GUILD_MESSAGES: 9,
+    GUILD_MESSAGE_REACTIONS: 10,
+    GUILD_MESSAGE_TYPING: 11,
+    DIRECT_MESSAGES: 12,
+    DIRECT_MESSAGE_REACTIONS: 13,
+    DIRECT_MESSAGE_TYPING: 14
+};
+module.exports.CALCULATED_INTENTS = (0b1 << this.INTENTS.GUILDS) |
+    (0b1 << this.INTENTS.GUILD_MEMBERS) |
+    (0b1 << this.INTENTS.GUILD_BANS) |
+    (0b1 << this.INTENTS.GUILD_INVITES) |
+    (0b1 << this.INTENTS.GUILD_VOICE_STATES) |
+    (0b1 << this.INTENTS.GUILD_MESSAGES);
