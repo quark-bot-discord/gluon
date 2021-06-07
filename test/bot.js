6d158411f6de689937027de0e8804c056a3c9e1d
Starman <30315137+Starman3787@users.noreply.github.com>
2021-06-07 09:41:00 +0100

    Update bot.js

--- test/bot.js
+++ test/bot.js
@@ -3,7 +3,13 @@ const client = new Client();
 
 client.on("ready", () => {
     console.log("ready");
-    console.log(client.user);
+    // console.log(client.user);
+});
+
+client.on("messageCreate", message => {
+    console.log(message);
+
+    console.log(client.guilds.cache["721401585300930562"]);
 });
 
 client.login("ODQ5NzM0MjM0MzM5NjcyMDg0.YLferA.RYFIAP-qz_U-wJB-qXmTD87p5gA");
