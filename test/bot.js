178ad79a48dd0c4ac96129993b46754f0a239c93
Starman <30315137+Starman3787@users.noreply.github.com>
2021-06-07 23:39:51 +0100

    update to test new features added to lib

--- test/bot.js
+++ test/bot.js
@@ -1,4 +1,7 @@
 const Client = require("../src/index");
+const ActionRow = require("../src/util/actionRowBuilder");
+const Component = require("../src/util/componentBuilder");
+const MessageComponents = require("../src/util/messageComponents");
 const client = new Client();
 
 client.on("ready", () => {
@@ -7,22 +10,51 @@ client.on("ready", () => {
 });
 
 client.on("raw", raw => {
+    console.log("raw");
     console.log(raw);
 });
 
 client.on("debug", data => {
+    console.log("debug");
     console.log(data);
 });
 
 client.on("messageCreate", message => {
     console.log("messageCreate");
     console.log(message);
+    if (message.author.bot == true) return;
+    if (message.content == "amogus")
+        message.reply("https://www.youtube.com/watch?v=1KaecOQKNaM");
+    if (message.content == "button") {
+        const actionRow = new ActionRow();
+        const button = new Component()
+            .setLabel("button")
+            .setStyle(1)
+            .setCustomID(message.guild.id);
+        actionRow.addComponent(button);
+        const messageComponents = new MessageComponents()
+            .addActionRow(actionRow);
+        message.reply("button", {
+            components: messageComponents
+        })
+        .then(m => {
+            console.log(m);
+        })
+        .catch(e => {
+            console.log(e.errors);
+        });
+    }
 });
 
 client.on("messageUpdate", (oldMessage, newMessage) => {
     console.log("messageUpdate");
     console.log(oldMessage);
     console.log(newMessage);
+});
+
+client.on("messageDelete", message => {
+    console.log("messageDelete");
+    console.log(message);
 });
 
 client.login("ODQ5NzM0MjM0MzM5NjcyMDg0.YLferA.RYFIAP-qz_U-wJB-qXmTD87p5gA");
