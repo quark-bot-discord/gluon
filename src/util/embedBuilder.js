6fa99c070bc427166ef6676866486008ebb13b08
Starman <30315137+Starman3787@users.noreply.github.com>
2021-06-07 23:52:34 +0100

    add embed builder

---
+++ src/util/embedBuilder.js
@@ -0,0 +1,107 @@
+const hexToInt = require("./hexToInt");
+
+class Embed {
+
+    constructor() {
+
+        this.type = "rich";
+        this.fields = [];
+
+    }
+
+    setTitle(title) {
+
+        this.title = title;
+
+        return this;
+
+    }
+
+    setDescription(text) {
+
+        this.description = text;
+
+        return this;
+
+    }
+
+    setURL(url) {
+
+        this.url = url;
+
+        return this;
+
+    }
+
+    setTimestamp(timestamp) {
+
+        this.timestamp = new Date(timestamp * 1000).toISOString();
+
+        return this;
+
+    }
+
+    setColor(hex) {
+        
+        this.color = hexToInt(hex);
+
+        return this;
+
+    }
+
+    setFooter(text, icon) {
+
+        this.footer = {
+            text: text
+        };
+        if (icon)
+            this.footer.icon_url = icon;
+
+        return this;
+    }
+
+    setAuthor(name, url, icon_url) {
+
+        this.author = {};
+
+        if (name)
+            this.author.name = name;
+        if (url)
+            this.author.url = url;
+        if (icon_url)
+            this.author.icon_url = icon_url;
+
+        return this;
+
+    }
+
+    addField(name, value, inline = false) {
+
+        this.fields.push({
+            name: name,
+            value: value,
+            inline: inline
+        });
+
+        return this;
+
+    }
+
+    toJSON() {
+
+        return {
+            title: this.title,
+            type: this.type,
+            description: this.description,
+            url: this.url,
+            timestemp: this.timestamp,
+            color: this.color,
+            footer: this.footer,
+            author: this.author,
+            fields: this.fields
+        };
+
+    }
+}
+
+module.exports = Embed;
