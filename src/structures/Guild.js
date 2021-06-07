a231d828d950c7ee8c1a50d69f9b66d6a0d2c067
Starman <30315137+Starman3787@users.noreply.github.com>
2021-06-07 09:40:44 +0100

    Create Guild.js

---
+++ src/structures/Guild.js
@@ -0,0 +1,41 @@
+const GuildChannelsManager = require("../managers/GuildChannelsManager");
+const GuildMemberManager = require("../managers/GuildMemberManager");
+const GuildThreadsManager = require("../managers/GuildThreadsManager");
+const GuildVoiceStatesManager = require("../managers/GuildVoiceStatesManager");
+
+class Guild {
+
+    constructor(client, data) {
+
+        this.id = data.id;
+
+        this.name = data.name;
+
+        this.icon = data.icon;
+
+        this.owner_id = data.owner_id;
+
+        this.joined_at = data.joined_at || null;
+
+        if (data.unavailable == true)
+            this.unavailable = data.unavailable;
+
+        this.member_count = data.member_count || null;
+
+        this.voice_states = new GuildVoiceStatesManager(data.voice_states);
+
+        this.members = new GuildMemberManager(data.members);
+
+        this.channels = new GuildChannelsManager(data.channels);
+
+        this.threads = new GuildThreadsManager(data.threads);
+
+        this.preferred_locale = data.preferred_locale;
+
+        client.guilds.cache[this.id] = this;
+
+    }
+
+}
+
+module.exports = Guild;
