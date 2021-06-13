96d93e615b2917ee8267b223c368071d581c2975
Starman <30315137+Starman3787@users.noreply.github.com>
2021-06-13 02:32:19 +0100

    add channel fetch method

--- src/managers/GuildChannelsManager.js
+++ src/managers/GuildChannelsManager.js
@@ -1,14 +1,46 @@
+const { CHANNEL_TYPES } = require("../constants");
+const TextChannel = require("../structures/TextChannel");
+const Thread = require("../structures/Thread");
+const VoiceChannel = require("../structures/VoiceChannel");
+
 class GuildChannelsManager {
 
-    constructor(client) {
+    constructor(client, guild) {
 
         this.client = client;
+
+        this.guild = guild;
 
         this.cache = {};
 
     }
 
-    async fetch() {
+    async fetch(channel_id) {
+
+        try {
+
+            const data = this.client.request.makeRequest("getChannel", [channel_id]);
+            switch (data.type) {
+                case CHANNEL_TYPES.GUILD_TEXT:
+                case CHANNEL_TYPES.GUILD_NEWS: {
+                    return new TextChannel(this.client, data, this.guild.id);
+                }
+                case CHANNEL_TYPES.GUILD_VOICE:
+                case CHANNEL_TYPES.GUILD_STAGE_VOICE: {
+                    return new VoiceChannel(this.client, data, this.guild.id);
+                }
+                case CHANNEL_TYPES.GUILD_NEWS_THREAD:
+                case CHANNEL_TYPES.GUILD_PUBLIC_THREAD:
+                case CHANNEL_TYPES.GUILD_PRIVATE_THREAD: {
+                    return new Thread(this.client, data, this.guild.id);
+                }
+            }
+
+        } catch (error) {
+
+            throw error;
+
+        }
 
     }
 
