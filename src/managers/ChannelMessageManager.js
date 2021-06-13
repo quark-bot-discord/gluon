6a2b6e72e8ec785b76e2f31c1c464a35050feece
Starman <30315137+Starman3787@users.noreply.github.com>
2021-06-13 11:46:29 +0100

    Update ChannelMessageManager.js

--- src/managers/ChannelMessageManager.js
+++ src/managers/ChannelMessageManager.js
@@ -1,8 +1,63 @@
+const Message = require("../structures/Message");
+
 class ChannelMessageManager {
 
-    constructor() {
+    constructor(client, channel) {
+
+        this.client = client;
+
+        this.channel = channel;
 
         this.cache = {};
+
+    }
+
+    async fetch(options) {
+
+        if (typeof options == "object") {
+
+            const body = {};
+
+            if (options.around)
+                body.around = options.around;
+
+            if (options.before)
+                body.before = options.before;
+
+            if (options.after)
+                body.after = options.after;
+
+            if (options.limit)
+                body.limit = options.limit;
+
+            try {
+
+                const data = await this.client.request.makeRequest("getChannelMessages", [this.channel.id], body);
+                let messages = [];
+                for (let i = 0; i < data.length; i++)
+                    messages.push(new Message(this.client, data[i], data[i].channel_id, this.channel.guild.id));
+                return messages;
+
+            } catch (error) {
+
+                throw error;
+
+            }
+
+        } else if (typeof options == "string") {
+
+            try {
+
+                const data = await this.client.request.makeRequest("getChannelMessage", [this.channel.id, options]);
+                return new Message(this.client, data, this.channel);
+
+            } catch (error) {
+
+                throw error;
+
+            }
+
+        }
 
     }
 
