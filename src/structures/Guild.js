492e2588388b19776d3c32c64af068e69ace4c5b
Starman <30315137+Starman3787@users.noreply.github.com>
2021-06-13 02:33:43 +0100

    add fetch audit log method

--- src/structures/Guild.js
+++ src/structures/Guild.js
@@ -1,8 +1,9 @@
-const { CHANNEL_TYPES } = require("../constants");
+const { CHANNEL_TYPES, AUDIT_LOG_TYPES } = require("../constants");
 const GuildChannelsManager = require("../managers/GuildChannelsManager");
 const GuildMemberManager = require("../managers/GuildMemberManager");
 const GuildThreadsManager = require("../managers/GuildThreadsManager");
 const GuildVoiceStatesManager = require("../managers/GuildVoiceStatesManager");
+const AuditLog = require("./AuditLog");
 const Channel = require("./Channel");
 const Member = require("./Member");
 const TextChannel = require("./TextChannel");
@@ -36,7 +37,7 @@ class Guild {
 
         this.members = new GuildMemberManager(client);
 
-        this.channels = new GuildChannelsManager(client);
+        this.channels = new GuildChannelsManager(client, this);
 
         this.threads = new GuildThreadsManager(client);
 
@@ -74,6 +75,7 @@ class Guild {
         data.threads.map(thread => new Thread(this.client, thread, this.id));
 
     }
+
     async ban(user_id, options = {}) {
         if (!user_id) throw Error('No userID was provided');
         const body = {};
@@ -84,14 +86,42 @@ class Guild {
 
         try {
 
-            const data = await this.client.request.makeRequest("putCreateGuildBan", [this.id, user_id], body);
+            await this.client.request.makeRequest("putCreateGuildBan", [this.id, user_id], body);
             return;
 
         } catch (error) {
-            console.log(error)
+
             throw error;
 
         }
+    }
+
+    async fetchAuditLogs(options) {
+
+        const body = {};
+
+        if (options.limit)
+            body.limit = options.limit;
+        else
+            body.limit = 1;
+
+        if (options.type)
+            body.type = options.type;
+
+        try {
+
+            const data = await this.client.request.makeRequest("getGuildAuditLog", [this.id], body);
+            if (options.type && AUDIT_LOG_TYPES[options.type] && data.action_type != AUDIT_LOG_TYPES[options.type])
+                return null;
+            // currently only one log is ever fetched, but best to make it easy to allow multiple if needed in the future
+            return [new AuditLog(this.client, data)];
+
+        } catch (error) {
+
+            throw error;
+
+        }
+
     }
 
 }
