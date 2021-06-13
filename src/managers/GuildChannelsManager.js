81db379817845f72d4e64b189191290fb4ca9da4
Starman <30315137+Starman3787@users.noreply.github.com>
2021-06-13 11:46:17 +0100

    Update GuildChannelsManager.js

--- src/managers/GuildChannelsManager.js
+++ src/managers/GuildChannelsManager.js
@@ -19,7 +19,7 @@ class GuildChannelsManager {
 
         try {
 
-            const data = this.client.request.makeRequest("getChannel", [channel_id]);
+            const data = await this.client.request.makeRequest("getChannel", [channel_id]);
             switch (data.type) {
                 case CHANNEL_TYPES.GUILD_TEXT:
                 case CHANNEL_TYPES.GUILD_NEWS: {
