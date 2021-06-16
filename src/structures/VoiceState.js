ae8e82e26d84e173ce5fe0ef2a3f13a214aae341
Starman <30315137+Starman3787@users.noreply.github.com>
2021-06-16 21:52:53 +0100

    Create VoiceState.js

---
+++ src/structures/VoiceState.js
@@ -0,0 +1,31 @@
+class VoiceState {
+
+    constructor(client, data, guild_id) {
+
+        this.client = client;
+
+        this.guild = this.client.guilds.cache.get(guild_id);
+
+        this.channel = this.guild.channels.cache.get(data.channel_id);
+
+        this.deaf = data.deaf;
+
+        this.mute = data.mute;
+
+        this.self_deaf = data.self_deaf;
+
+        this.self_mute = data.self_mute;
+
+        this.self_video = data.self_video;
+
+        this.member = this.guild.members.cache.get(data.user_id);
+
+        this.user = this.client.users.cache.get(data.user_id);
+
+        this.guild.voice_states.cache.set(data.user_id, this);
+
+    }
+
+}
+
+module.exports = VoiceState;
