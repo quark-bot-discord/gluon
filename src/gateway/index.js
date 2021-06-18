37f407446a752414a36d10d6d8e3cdce2bef4e27
Starman <30315137+Starman3787@users.noreply.github.com>
2021-06-18 20:58:54 +0100

    try to fix resume and add time to debug logging

--- src/gateway/index.js
+++ src/gateway/index.js
@@ -44,17 +44,161 @@ class WS {
             chunkSize: 128 * 1024
         });
 
+        this.addListeners();
+
+    }
+
+    handleIncoming(data) {
+
+        if (!data) return;
+
+        if (data.s) this.s = data.s;
+
+        this.client.emit("raw", data);
+
+        switch (data.op) {
+            // Dispatch
+            case 0: {
+
+                try {
+
+                    this.eventHandler[data.t](data.d);
+
+                } catch (error) {
+                    console.log(error);
+                    this.client.emit("debug", `${this.libName} ${this.shardWarning} @ ${this.time()} => Unknown event ${data.t}`);
+
+                }
+
+                break;
+
+            }
+            // Heartbeat
+            case 1: {
+
+                this.heartbeat();
+
+                break;
+
+            }
+            // Reconnect
+            case 7: {
+
+                // reconnect to websocket with session id
+                this.reconnect();
+
+                break;
+
+            }
+            // Invalid Session
+            case 9: {
+
+                this.client.emit("debug", `${this.libName} ${this.shardCatastrophic} @ ${this.time()} => INVALID SESSION`);
+
+                break;
+
+            }
+            // Hello
+            case 10: {
+
+                this.heartbeat();
+
+                this.hearbeatSetInterval = setInterval((() => {
+
+                    this.heartbeat();
+
+                }), data.d.heartbeat_interval);
+
+                this.client.emit("debug", `${this.libName} ${this.shardNorminal} @ ${this.time()} => HELLO`);
+
+                break;
+
+            }
+            // Heartbeat ACK
+            case 11: {
+
+                if (this.isInitialHeartbeat == true) {
+
+                    this.isInitialHeartbeat = false;
+
+                    this.identify();
+
+                } else if (this.resuming == true) {
+
+                    this.resume();
+
+                }
+
+
+                this.client.emit("debug", `${this.libName} ${this.shardNorminal} @ ${this.time()} => Hearbeat acknowledged`);
+
+                break;
+
+            }
+
+        }
+
+    }
+
+    heartbeat() {
+
+        this.client.emit("debug", `${this.libName} ${this.shardNorminal} @ ${this.time()} => Sending heartbeat...`);
+
+        this.ws.send(new Heartbeat(this.s));
+
+    }
+
+    identify() {
+
+        this.client.emit("debug", `${this.libName} ${this.shardNorminal} @ ${this.time()} => IDENTIFY`);
+
+        this.ws.send(new Identify(this.token, this.shard));
+
+    }
+
+    reconnect() {
+
+        clearInterval(this.hearbeatSetInterval);
+
+        this.resuming = true;
+
+        this.ws.close();
+
+        this.client.emit("debug", `${this.libName} ${this.shardWarning} @ ${this.time()} => Shard reconnecting`);
+
+    }
+
+    resume() {
+
+        this.resuming = false;
+
+        this.client.emit("debug", `${this.libName} ${this.shardWarning} @ ${this.time()} => RESUMING`);
+
+        this.ws.send(new Resume(this.token, this.sessionId, this.s));
+
+    }
+
+    time() {
+
+        return chalk.inverse(new Date().toGMTString());
+
+    }
+
+    addListeners() {
+
         this.ws.on("open", () => {
 
-            this.client.emit("debug", `${this.libName} ${this.shardNorminal} => Websocket opened`);
+            this.client.emit("debug", `${this.libName} ${this.shardNorminal} @ ${this.time()} => Websocket opened`);
 
         });
 
         this.ws.on("close", data => {
 
-            this.client.emit("debug", `${this.libName} ${data < 2000 ? this.shardNorminal : this.shardCatastrophic} => Websocket closed with code ${data}`);
+            this.client.emit("debug", `${this.libName} ${data < 2000 ? this.shardNorminal : this.shardCatastrophic} @ ${this.time()} => Websocket closed with code ${data}`);
 
             this.ws = new WebSocket(this.url);
+
+            this.addListeners();
 
         });
 
@@ -90,136 +234,6 @@ class WS {
             this.client.emit("error", `error: ${data}`);
 
         });
-
-    }
-
-    handleIncoming(data) {
-
-        if (!data) return;
-
-        if (data.s) this.s = data.s;
-
-        this.client.emit("raw", data);
-
-        switch (data.op) {
-            // Dispatch
-            case 0: {
-
-                try {
-
-                    this.eventHandler[data.t](data.d);
-
-                } catch (error) {
-                    console.log(error);
-                    this.client.emit("debug", `${this.libName} ${this.shardWarning} => Unknown event ${data.t}`);
-
-                }
-
-                break;
-
-            }
-            // Heartbeat
-            case 1: {
-
-                this.heartbeat();
-
-                break;
-
-            }
-            // Reconnect
-            case 7: {
-
-                // reconnect to websocket with session id
-                this.reconnect();
-
-                break;
-
-            }
-            // Invalid Session
-            case 9: {
-
-                this.client.emit("debug", `${this.libName} ${this.shardCatastrophic} => INVALID SESSION`);
-
-                break;
-
-            }
-            // Hello
-            case 10: {
-
-                this.heartbeat();
-
-                this.hearbeatSetInterval = setInterval((() => {
-
-                    this.heartbeat();
-
-                }), data.d.heartbeat_interval);
-
-                this.client.emit("debug", `${this.libName} ${this.shardNorminal} => HELLO`);
-
-                break;
-
-            }
-            // Heartbeat ACK
-            case 11: {
-
-                if (this.isInitialHeartbeat == true) {
-
-                    this.isInitialHeartbeat = false;
-
-                    this.identify();
-
-                } else if (this.resuming == true) {
-
-                    this.resume();
-
-                }
-
-
-                this.client.emit("debug", `${this.libName} ${this.shardNorminal} => Hearbeat acknowledged`);
-
-                break;
-
-            }
-
-        }
-
-    }
-
-    heartbeat() {
-
-        this.client.emit("debug", `${this.libName} ${this.shardNorminal} => Sending heartbeat...`);
-
-        this.ws.send(new Heartbeat(this.s));
-
-    }
-
-    identify() {
-
-        this.client.emit("debug", `${this.libName} ${this.shardNorminal} => IDENTIFY`);
-
-        this.ws.send(new Identify(this.token, this.shard));
-
-    }
-
-    reconnect() {
-
-        clearInterval(this.hearbeatSetInterval);
-
-        this.resuming = true;
-
-        this.ws.close();
-
-        this.client.emit("debug", `${this.libName} ${this.shardWarning} => Shard reconnecting`);
-
-    }
-
-    resume() {
-
-        this.resuming = false;
-
-        this.client.emit("debug", `${this.libName} ${this.shardWarning} => RESUMING`);
-
-        this.ws.send(new Resume(this.token, this.sessionId, this.s));
 
     }
 
