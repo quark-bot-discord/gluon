6cfe094e8afe477d4541791d27a2647a42ed1fc7
Misly <60405462+Misly16@users.noreply.github.com>
2021-06-20 17:23:19 +0100

    added user flags

--- src/constants.js
+++ src/constants.js
@@ -43,6 +43,22 @@ module.exports.CHANNEL_TYPES = {
     GUILD_PRIVATE_THREAD: 12,
     GUILD_STAGE_VOICE: 13
 };
+module.exports.USER_FLAGS = {
+    NONE:                   0,
+    DISCORD_EMPLOYEE:       1 << 0,
+    DISCORD_PARTNER:        1 << 1,
+    HYPESQUAD_EVENTS:       1 << 2,
+    BUG_HUNTER_LEVEL_1:     1 << 3,
+    HOUSE_BRAVERY:          1 << 6,
+    HOUSE_BRILLIANCE:       1 << 7,
+    HOUSE_BALANCE:          1 << 8,
+    EARLY_SUPPORTER:        1 << 9,
+    TEAM_USER:              1 << 10,
+    SYSTEM:                 1 << 12,
+    BUG_HUNTER_LEVEL_2:     1 << 14,
+    VERIFIED_BOT:           1 << 16,
+    VERIFIED_BOT_DEVELOPER: 1 << 17
+};
 module.exports.AUDIT_LOG_TYPES = {
     MEMBER_KICK: 20,
     MEMBER_BAN_ADD: 22,
