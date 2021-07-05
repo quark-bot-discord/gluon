2390272f4210e650d1c00c00d62a5dd54c5c8f0d
Starman <30315137+Starman3787@users.noreply.github.com>
2021-07-05 20:44:49 +0100

    add interaction class

--- src/structures/ButtonClick.js
+++ src/structures/ButtonClick.js
@@ -1,67 +1,15 @@
-const Member = require("./Member");
+const Interaction = require("./Interaction");
 const Message = require("./Message");
-const User = require("./User");
 
-class ButtonClick {
+class ButtonClick extends Interaction {
 
     constructor(client, data) {
 
-        this.client = client;
-
-        this.id = BigInt(data.id);
-
-        this.type = data.type;
+        super(client, data);
 
         this.custom_id = data.data.custom_id;
 
-        this.guild = this.client.guilds.cache.get(data.guild_id) || null;
-
-        if (!this.guild)
-            this.guild_id = BigInt(data.guild_id);
-
-        this.channel = this.guild?.channels.cache.get(data.channel_id) || null;
-
-        if (!this.channel)
-            this.channel_id = BigInt(data.channel_id);
-
-        if (data.member)
-            this.member = new Member(this.client, data.member, data.member.user.id, data.guild_id);
-
-        if (data.user)
-            this.user = new User(this.client, data.user);
-
-        this.token = data.token;
-
         this.message = new Message(this.client, data.message, data.channel_id, data.guild_id, this.client.cacheMessages);
-
-    }
-
-    async reply(content, { embed, components, quiet } = {}) {
-
-        const body = {};
-
-        body.type = 4;
-        body.data = {};
-
-        if (content)
-            body.data.content = content;
-        if (embed)
-            body.data.embed = embed.toJSON();
-        if (components)
-            body.data.components = components.toJSON();
-        if (quiet == true)
-            body.data.flags = 64;
-
-        try {
-
-            await this.client.request.makeRequest("postInteractionResponse", [this.id, this.token], body);
-
-        } catch (error) {
-
-            this.client.error(error.stack.toString());
-            throw error;
-
-        }
 
     }
 
