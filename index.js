78361681c8dc8bfd1f6a39ba420987dd0bccc789
Starman <30315137+Starman3787@users.noreply.github.com>
2021-07-15 19:16:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,22 @@ const Dropdown = require("./src/util/dropdownBuilder");
 const DropdownOption = require("./src/util/dropdownOption");
 const Embed = require("./src/util/embedBuilder");
 const MessageComponents = require("./src/util/messageComponents");
+const Attachment = require("./src/structures/Attachment");
+const AuditLog = require("./src/structures/AuditLog");
+const ButtonClick = require("./src/structures/ButtonClick");
+const Channel = require("./src/structures/Channel");
+const Guild = require("./src/structures/Guild");
+const Interaction = require("./src/structures/Interaction");
+const Member = require("./src/structures/Member");
+const Message = require("./src/structures/Message");
+const OptionSelect = require("./src/structures/OptionSelect");
+const Role = require("./src/structures/Role");
+const SlashCommand = require("./src/structures/SlashCommand");
+const TextChannel = require("./src/structures/TextChannel");
+const Thread = require("./src/structures/Thread");
+const User = require("./src/structures/User");
+const VoiceChannel = require("./src/structures/VoiceChannel");
+const VoiceState = require("./src/structures/VoiceState");
 const { PERMISSIONS } = require("./src/constants");
 
 module.exports = {
@@ -15,5 +31,21 @@ module.exports = {
     DropdownOption,
     Embed,
     MessageComponents,
+    Attachment,
+    AuditLog,
+    ButtonClick,
+    Channel,
+    Guild,
+    Interaction,
+    Member,
+    Message,
+    OptionSelect,
+    Role,
+    SlashCommand,
+    TextChannel,
+    Thread,
+    User,
+    VoiceChannel,
+    VoiceState,
     PERMISSIONS
 };
