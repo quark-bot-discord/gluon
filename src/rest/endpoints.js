237c08e5ba3b8b8e1c278913b2e2fae0f2683ab1
Starman <30315137+Starman3787@users.noreply.github.com>
2021-07-28 12:40:21 +0100

    remove change nickname method

--- src/rest/endpoints.js
+++ src/rest/endpoints.js
@@ -182,12 +182,5 @@ module.exports = {
         },
         method: "GET",
         bucket: null
-    },
-    patchModifyGuildMember: {
-        path: ([guild_id, user_id]) => {
-            return `/guilds/${guild_id}/members/${user_id}`;
-        },
-        method: "PATCH",
-        bucket: null
     }
 };
