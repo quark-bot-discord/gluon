1d346e07a79063a007680d1cdd27b22f57226ac2
Starman <30315137+Starman3787@users.noreply.github.com>
2021-07-28 13:53:15 +0100

    Update resolveEmoji.js

--- src/util/resolveEmoji.js
+++ src/util/resolveEmoji.js
@@ -1,8 +1,9 @@
+/* eslint-disable quotes */
 function resolveEmoji(text) {
     const emojis = text.match(/<:[^:\s]+:[0-9]+>|<a:[^:\s]+:[0-9]+>/g);
     if (!emojis || emojis.length == 0)
         return null;
-    const splitEmoji = emojis[0].replace(/<|>/g, "").split(":");
+    const splitEmoji = emojis[0].replace(/<|>/g, '').split(':');
     return {
         id: splitEmoji[2],
         name: splitEmoji[1],
