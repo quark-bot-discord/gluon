385afb20b4fc4d5466dc6383ce656d1d6dd34631
Starman <30315137+Starman3787@users.noreply.github.com>
2021-07-31 14:23:46 +0100

    check for admin

--- src/util/checkPermission.js
+++ src/util/checkPermission.js
@@ -1,5 +1,9 @@
+const { PERMISSIONS } = require("../constants");
+
 function checkPermission(member, permission) {
     if (!member.permissions || !permission)
+        return true;
+    if ((member.permissions & PERMISSIONS.ADMINISTRATOR) == PERMISSIONS.ADMINISTRATOR)
         return true;
     return (member.permissions & permission) == permission;
 }
