cc8bb7a048aec7070358d198bf75883a7b3a227b
Starman <30315137+Starman3787@users.noreply.github.com>
2021-08-13 16:56:53 +0100

    Update checkPermission.js

--- src/util/checkPermission.js
+++ src/util/checkPermission.js
@@ -1,7 +1,9 @@
 const { PERMISSIONS } = require("../constants");
 
 function checkPermission(member, permission) {
-    if (!member.permissions || !permission)
+    if (!permission)
+        return true;
+    if (member.guild?.owner_id == member.id)
         return true;
     member.permissions = BigInt(member.permissions);
     if ((member.permissions & PERMISSIONS.ADMINISTRATOR) == PERMISSIONS.ADMINISTRATOR)
