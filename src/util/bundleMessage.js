e481f3a016f0ad1af19fe0e04ddb529fc727285f
Starman <30315137+Starman3787@users.noreply.github.com>
2021-09-08 00:43:30 +0100

    Update bundleMessage.js

--- src/util/bundleMessage.js
+++ src/util/bundleMessage.js
@@ -16,7 +16,9 @@ function bundleMessage(message) {
     data.embeds = message.embeds;
     data.type = message.type;
     data.referenced_message = {};
-    data.referenced_message.id = message.reference?.message_id?.toString() || {};
+    const referencedMessageId = message.reference?.message_id?.toString();
+    if (referencedMessageId)
+        data.referenced_message.id = referencedMessageId;
     data.sticker_items = [];
     for (let i = 0; i < message.sticker_items.length; i++)
         data.sticker_items.push(bundleSticker(message.sticker_items[i]));
