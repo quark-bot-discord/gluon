f7fc0590f3d381446f8ec08971a71606fed77518
Starman <30315137+Starman3787@users.noreply.github.com>
2022-05-24 21:02:23 +0100

    Update resolveEmoji.js

--- src/util/resolveEmoji.js
+++ src/util/resolveEmoji.js
@@ -1,14 +1,19 @@
 /* eslint-disable quotes */
 function resolveEmoji(text) {
+
     const emojis = text.match(/<:[^:\s]+:[0-9]+>|<a:[^:\s]+:[0-9]+>/g);
+
     if (!emojis || emojis.length == 0)
         return null;
+
     const splitEmoji = emojis[0].replace(/<|>/g, '').split(':');
+
     return {
         id: splitEmoji[2],
         name: splitEmoji[1],
         animated: splitEmoji[0] == 'a' ? true : false
     };
+
 }
 
 module.exports = resolveEmoji;
