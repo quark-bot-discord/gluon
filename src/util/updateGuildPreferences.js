7ef2c43e652666d5aced9de2943cc7bce28e055d
Starman <30315137+Starman3787@users.noreply.github.com>
2024-01-16 22:39:18 +0000

    Update updateGuildPreferences.js

--- src/util/updateGuildPreferences.js
+++ src/util/updateGuildPreferences.js
@@ -18,24 +18,25 @@ function updateSingleGuildPreferences(client, guild) {
         return;
     currentGuild._cache_options = 0;
     if (guild.options.serverLog == null) {
-        if (!guild.options.additionalServerLogOptions.textEvents)
+        if (!guild.options?.additionalServerLogOptions?.textEvents)
             if (guild.options.additionalServerLogOptions.fileEvents)
                 currentGuild._cache_options |= GLUON_CACHING_OPTIONS.FILES_ONLY;
             else
                 currentGuild._cache_options |= GLUON_CACHING_OPTIONS.NO_MESSAGES;
             
-        if (!guild.options.additionalServerLogOptions.voiceEvents)
+        if (!guild.options?.additionalServerLogOptions?.voiceEvents)
             currentGuild._cache_options |= GLUON_CACHING_OPTIONS.NO_VOICE_STATE;
     }
     client.guilds.cache.set(guild.id, currentGuild);
-    for (let j = 0; j < guild.options.additionalServerLogOptions.ignoreChannels.length; j++) {
-        let currentGuildChannel = currentGuild.channels.cache.get(guild.options.additionalServerLogOptions.ignoreChannels[j])
-        if (!currentGuildChannel)
-            continue;
-        currentGuildChannel._cache_options = 0;
-        currentGuildChannel._cache_options |= GLUON_CACHING_OPTIONS.NO_MESSAGES;
-        currentGuild.channels.cache.set(guild.options.additionalServerLogOptions.ignoreChannels[j], currentGuildChannel);
-    }
+    if (guild.options?.additionalServerLogOptions?.ignoreChannels)
+        for (let j = 0; j < guild.options.additionalServerLogOptions.ignoreChannels.length; j++) {
+            let currentGuildChannel = currentGuild.channels.cache.get(guild.options.additionalServerLogOptions.ignoreChannels[j])
+            if (!currentGuildChannel)
+                continue;
+            currentGuildChannel._cache_options = 0;
+            currentGuildChannel._cache_options |= GLUON_CACHING_OPTIONS.NO_MESSAGES;
+            currentGuild.channels.cache.set(guild.options.additionalServerLogOptions.ignoreChannels[j], currentGuildChannel);
+        }
 }
 
 module.exports = updatePreferences;
