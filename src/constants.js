2834af3a4192d0212560ce027d8e78f933a0d805
Starman <30315137+Starman3787@users.noreply.github.com>
2024-03-05 23:24:44 +0000

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -171,6 +171,24 @@ module.exports.COMPONENT_TYPES = {
     MENTIONABLE_SELECT_MENU:    (7),
     CHANNEL_SELECT_MENU:        (8)
 };
+module.exports.APPLICATION_COMMAND_TYPES = {
+    CHAT_INPUT:                 (1),
+    USER:                       (2),
+    MESSAGE:                    (3)
+};
+module.exports.APPLICATION_COMMAND_OPTION_TYPES = {
+    SUB_COMMAND:                (1),
+    SUB_COMMAND_GROUP:          (2),
+    STRING:                     (3),
+    INTEGER:                    (4),
+    BOOLEAN:                    (5),
+    USER:                       (6),
+    CHANNEL:                    (7),
+    ROLE:                       (8),
+    MENTIONABLE:                (9),
+    NUMBER:                     (10),
+    ATTACHMENT:                 (11)
+};
 module.exports.DEFAULT_MESSAGE_EXPIRY_SECONDS = 3600 * 6; // 6 hours
 module.exports.DEFAULT_USER_EXPIRY_SECONDS = 3600 * 1; // 1 hour
 module.exports.PERMISSIONS = {
