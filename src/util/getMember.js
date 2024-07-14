5ab4e758e56da08f31035d6274bfa07a95e16176
Starman <30315137+Starman3787@users.noreply.github.com>
2024-07-14 14:43:36 +0100

    Update getMember.js

--- src/util/getMember.js
+++ src/util/getMember.js
@@ -1,27 +1,25 @@
-function getMember(client, guild_id, member_id, destroy = false) {
-  return new Promise(async (resolve, reject) => {
-    const guild = client.guilds.cache.get(guild_id);
+async function getMember(client, guild_id, member_id, destroy = false) {
+  const guild = client.guilds.cache.get(guild_id);
 
-    const member = guild.members.cache.get(member_id) || null;
+  const member = guild.members.cache.get(member_id) || null;
 
-    if (!member) {
-      const storedMember = await guild.members.retrieve(member_id);
+  if (!member) {
+    const storedMember = await guild.members.retrieve(member_id);
 
-      if (storedMember) {
-        guild.members.remove(member_id);
-
-        if (destroy != false) guild.members.cache.delete(member_id);
-
-        return resolve(storedMember);
-      } else return resolve(null);
-    } else {
+    if (storedMember) {
       guild.members.remove(member_id);
 
       if (destroy != false) guild.members.cache.delete(member_id);
 
-      return resolve(member);
-    }
-  });
+      return storedMember;
+    } else return null;
+  } else {
+    guild.members.remove(member_id);
+
+    if (destroy != false) guild.members.cache.delete(member_id);
+
+    return member;
+  }
 }
 
 module.exports = getMember;
