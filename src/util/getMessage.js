c66c6628eb26bb74792809b9b4afc67461ec4ff4
Starman <30315137+Starman3787@users.noreply.github.com>
2024-07-14 14:43:34 +0100

    Update getMessage.js

--- src/util/getMessage.js
+++ src/util/getMessage.js
@@ -9,64 +9,55 @@ const hash = require("hash.js");
  * @param {String} channel_id The id of the channel the message belongs to.
  * @param {String} message_id The id of the message to get.
  * @param {Boolean?} destroy Whether the message should be removed from the cache once it has been fetched.
- * @returns {Message?}
+ * @returns {Promise<Message?>} The message, if it can be found.
  */
-function getMessage(client, guild_id, channel_id, message_id, destroy = false) {
-  return new Promise(async (resolve, reject) => {
-    let message =
-      client.guilds.cache
-        .get(guild_id)
-        ?.channels.cache.get(channel_id)
-        ?.messages.cache.get(message_id) || null;
+async function getMessage(
+  client,
+  guild_id,
+  channel_id,
+  message_id,
+  destroy = false
+) {
+  let message =
+    client.guilds.cache
+      .get(guild_id)
+      ?.channels.cache.get(channel_id)
+      ?.messages.cache.get(message_id) || null;
 
-    const usedHash = hash
-      .sha512()
-      .update(`${guild_id}_${channel_id}_${message_id}`)
-      .digest("hex");
+  const usedHash = hash
+    .sha512()
+    .update(`${guild_id}_${channel_id}_${message_id}`)
+    .digest("hex");
 
-    const guildCacheMultiplier =
-      client.increasedCacheMultipliers.get(guild_id.toString()) || 1;
+  const guildCacheMultiplier =
+    client.increasedCacheMultipliers.get(guild_id.toString()) || 1;
 
-    if (
-      !message &&
-      client.increasedCache.get(guild_id) &&
-      getTimestamp(message_id) +
-        client.defaultMessageExpiry *
-          client.increaseCacheBy *
-          guildCacheMultiplier >
-        ((new Date().getTime() / 1000) |
-          0) /* && ((getTimestamp(message_id) + client.defaultMessageExpiry) < ((new Date().getTime() / 1000) | 0))*/
-    ) {
-      const rawMessage = await client.s3Messages
-        .getObject({ Bucket: client.s3MessageBucket, Key: usedHash })
-        .promise()
-        .catch(() => null);
-      if (!rawMessage || !rawMessage.Body) return resolve(null);
+  if (
+    !message &&
+    client.increasedCache.get(guild_id) &&
+    getTimestamp(message_id) +
+      client.defaultMessageExpiry *
+        client.increaseCacheBy *
+        guildCacheMultiplier >
+      ((new Date().getTime() / 1000) |
+        0) /* && ((getTimestamp(message_id) + client.defaultMessageExpiry) < ((new Date().getTime() / 1000) | 0))*/
+  ) {
+    const rawMessage = await client.s3Messages
+      .getObject({ Bucket: client.s3MessageBucket, Key: usedHash })
+      .promise()
+      .catch(() => null);
+    if (!rawMessage || !rawMessage.Body) return null;
 
-      const storedMessage = rawMessage.Body.toString();
-      if (storedMessage) {
-        message = decryptMessage(
-          client,
-          storedMessage,
-          message_id,
-          channel_id,
-          guild_id
-        );
+    const storedMessage = rawMessage.Body.toString();
+    if (storedMessage) {
+      message = decryptMessage(
+        client,
+        storedMessage,
+        message_id,
+        channel_id,
+        guild_id
+      );
 
-        client.s3Messages
-          .deleteObject({ Bucket: client.s3MessageBucket, Key: usedHash })
-          .promise()
-          .catch(() => null);
-
-        if (destroy != false)
-          client.guilds.cache
-            .get(guild_id)
-            ?.channels.cache.get(channel_id)
-            ?.messages.cache.delete(message_id);
-
-        return resolve(message);
-      } else return resolve(null);
-    } else {
       client.s3Messages
         .deleteObject({ Bucket: client.s3MessageBucket, Key: usedHash })
         .promise()
@@ -78,9 +69,22 @@ function getMessage(client, guild_id, channel_id, message_id, destroy = false) {
           ?.channels.cache.get(channel_id)
           ?.messages.cache.delete(message_id);
 
-      return resolve(message);
-    }
-  });
+      return message;
+    } else return null;
+  } else {
+    client.s3Messages
+      .deleteObject({ Bucket: client.s3MessageBucket, Key: usedHash })
+      .promise()
+      .catch(() => null);
+
+    if (destroy != false)
+      client.guilds.cache
+        .get(guild_id)
+        ?.channels.cache.get(channel_id)
+        ?.messages.cache.delete(message_id);
+
+    return message;
+  }
 }
 
 module.exports = getMessage;
