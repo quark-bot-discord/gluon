c04a95fff843c2b4a6f997dbbde6cbccb2b0f0d7
Starman <30315137+Starman3787@users.noreply.github.com>
2024-09-02 11:44:34 +0200

    add all GET requests to tests

---
+++ test/requests/GET/channels/[channel_id]/pins/request.js
@@ -0,0 +1,12 @@
+import { expect } from "chai";
+import { TEST_CLIENTS } from "../../../../../../src/testData.js";
+
+describe("GET /channels/${channel_id}/pins", function () {
+  context("make request", function () {
+    it("should make the request", async function () {
+      const client = TEST_CLIENTS.ALL_CACHES_ENABLED();
+      await expect(client.request.makeRequest("getChannelPins")).to.be
+        .fulfilled;
+    });
+  });
+});
---
+++ test/requests/GET/guilds/[guild_id]/bans/[user_id]/request.js
@@ -0,0 +1,11 @@
+import { expect } from "chai";
+import { TEST_CLIENTS } from "../../../../../../../src/testData.js";
+
+describe("GET /guilds/${guild_id}/bans/${user_id}", function () {
+  context("make request", function () {
+    it("should make the request", async function () {
+      const client = TEST_CLIENTS.ALL_CACHES_ENABLED();
+      await expect(client.request.makeRequest("getGuildBan")).to.be.fulfilled;
+    });
+  });
+});
