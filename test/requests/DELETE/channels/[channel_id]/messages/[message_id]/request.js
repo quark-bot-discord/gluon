e401212462ee932947ee103f607a3db8604da42c
Starman <30315137+Starman3787@users.noreply.github.com>
2024-09-02 22:04:27 +0200

    add delete requests

---
+++ test/requests/DELETE/channels/[channel_id]/messages/[message_id]/request.js
@@ -0,0 +1,12 @@
+import { expect } from "chai";
+import { TEST_CLIENTS } from "../../../../../../../src/testData.js";
+
+describe("DELETE /channels/${channel_id}/messages/${message_id}", function () {
+  context("make request", function () {
+    it("should make the request", async function () {
+      const client = TEST_CLIENTS.ALL_CACHES_ENABLED();
+      await expect(client.request.makeRequest("deleteChannelMessage")).to.be
+        .fulfilled;
+    });
+  });
+});
