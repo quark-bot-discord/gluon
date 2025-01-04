c95e3d96222c6fb1796c3937bbf0d21711319c37
Starman <30315137+Starman3787@users.noreply.github.com>
2025-01-04 00:01:22 +0000

    Update Message.js

--- src/structures/Message.js
+++ src/structures/Message.js
@@ -825,29 +825,19 @@ class Message {
    * @async
    * @throws {Error | TypeError}
    */
-  edit(
-    {
-      components,
-      files,
-      content = this.content,
-      embeds = this.embeds,
-      attachments = this.attachments,
-      flags = this.flagsRaw,
-      reference = {
-        message_id: this.reference.messageId,
-        channel_id: this.channelId,
-        guild_id: this.guildId,
-      },
-    } = {
-      components: null,
-      files: null,
-      content: null,
-      embeds: null,
-      attachments: null,
-      flags: null,
-      reference: null,
+  edit({
+    components,
+    files,
+    content = this.content,
+    embeds = this.embeds,
+    attachments = this.attachments,
+    flags = this.flagsRaw,
+    reference = {
+      message_id: this.reference.messageId,
+      channel_id: this.channelId,
+      guild_id: this.guildId,
     },
-  ) {
+  }) {
     return Message.edit(this.#_client, this.channelId, this.id, this.guildId, {
       content,
       components,
