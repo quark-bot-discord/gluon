16d7e71219fcc4187bb77db685bf95803d4b770a
Starman3787 <Starman3787@users.noreply.github.com>
2025-01-24 11:19:28 +0000

    Commit from GitHub Actions (Export and compile structures)

--- dist/src/util.d.ts
+++ dist/src/util.d.ts
@@ -20,4 +20,5 @@ export { default as verifyMessageLink } from "./util/discord/verifyMessageLink.j
 export { default as escapedString } from "./util/discord/escapedString.js";
 export { default as combinePermissions } from "./util/discord/combinePermissions.js";
 export { default as FileUpload } from "./util/builder/fileUpload.js";
+export { default as isValidUrl } from "./util/general/isValidUrl.js";
 //# sourceMappingURL=util.d.ts.map
